fc51bf7fa3901c362cbb4990dc2ab6e117840ceb
letusfly85 <letusfly95@gmail.com>
2018-07-18 00:05:13 +0900

    feat: add reserve table

---
+++ src/main/resources/db/migrations/V0.0.1__reserves.sql
@@ -0,0 +1,15 @@
+CREATE TABLE reserves (
+  id                INT AUTO_INCREMENT,
+  name              VARCHAR(300) NOT NULL,
+  description       VARCHAR(2000),
+  reserved_at       DATETIME NOT NULL,
+  job_id            VARCHAR(300) NOT NULL,
+  job_status        VARCHAR(300) NOT NULL DEFAULT 'PENDING',
+  serial_number     VARCHAR(300) NOT NULL,
+  executed_at       DATETIME,
+  cancled_at        DATETIME,
+  created_at        DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at        DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
+);
+
