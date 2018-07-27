d889fd5e66281eed143b84061ec1a066439850b4
letusfly85 <letusfly95@gmail.com>
2018-07-27 22:14:42 +0900

    feat: use routing file

--- src/main/scala/io/wonder/soft/reser/application/routes/ReserveRoute.scala
+++ src/main/scala/io/wonder/soft/reser/application/routes/ReserveRoute.scala
@@ -1,5 +1,51 @@
 package io.wonder.soft.reser.application.routes
 
-class ReserveRoute {
+
+import akka.http.scaladsl.server.Directives._
+import io.wonder.soft.reser.application.services.ReserveService
+
+import io.circe.generic.auto._
+import io.circe.syntax._
+
+import akka.http.scaladsl.model.StatusCodes
+import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport
+
+import scala.concurrent.ExecutionContext
+
+class ReserveRoute(reserveService: ReserveService)(implicit executionContext: ExecutionContext) extends FailFastCirceSupport {
+
+  val route = pathPrefix("reserves") {
+    pathEndOrSingleSlash {
+      get {
+        val reserves = reserveService.searchByUserId("")
+        val json = reserves.map(_.asJson).asJson
+        complete(json)
+      }
+    }
+    /*~
+      pathPrefix(Segment) { id =>
+        pathEndOrSingleSlash {
+          get {
+            complete(getProfile(id).map {
+              case Some(profile) =>
+                OK -> profile.asJson
+              case None =>
+                BadRequest -> None.asJson
+            })
+          } ~
+            post {
+              entity(as[UserProfileUpdate]) { userUpdate =>
+                complete(updateProfile(id, userUpdate).map {
+                  case Some(profile) =>
+                    OK -> profile.asJson
+                  case None =>
+                    BadRequest -> None.asJson
+                })
+              }
+            }
+        }
+      }
+      */
+  }
 
 }
