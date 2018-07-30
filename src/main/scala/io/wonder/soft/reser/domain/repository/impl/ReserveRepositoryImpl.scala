13e13279ec11d118184258f19d0d5aeef4f9ecf2
letusfly85 <letusfly95@gmail.com>
2018-07-31 00:36:46 +0900

    feat: implement destroy method

--- src/main/scala/io/wonder/soft/reser/domain/repository/impl/ReserveRepositoryImpl.scala
+++ src/main/scala/io/wonder/soft/reser/domain/repository/impl/ReserveRepositoryImpl.scala
@@ -7,7 +7,6 @@ import io.wonder.soft.reser.domain.repository.ReserveRepository
 import scala.concurrent.Await
 import scala.concurrent.duration._
 import io.getquill._
-import sun.reflect.generics.reflectiveObjects.NotImplementedException
 
 import scala.util.{Failure, Success, Try}
 
@@ -34,12 +33,23 @@ class ReserveRepositoryImpl extends DBConfig with ReserveRepository {
 
   def searchByUserId(userId: String): List[ReserveEntity] = {
     List.empty[ReserveEntity]
-
-    List(ReserveEntity(1, "test"))
   }
 
   def destroy(id: Int): Either[Exception, ReserveEntity] = {
-    Left(new NotImplementedException))
+    this.find(id).map{ entity =>
+      val result = this.run(quote {
+        reserves.filter(u => u.id == lift(id)).delete
+      })
+
+      Try {
+        Await.result(result, 5.seconds)
+        entity
+      } match {
+        case Success(entity) =>  return Right(entity)
+        case Failure(exception) => return Left(new RuntimeException(exception))
+      }
+
+    }.toRight(new Exception(s"reserves ${id} not found"))
   }
 
 }
