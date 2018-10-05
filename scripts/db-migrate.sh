d46919a671a4e1a2528e376154b1d3e528ce127d
letusfly85 <letusfly95@gmail.com>
2018-10-05 18:29:15 +0900

    fix: use CI env to handle docker network

--- scripts/db-migrate.sh
+++ scripts/db-migrate.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
 export ROOT_PATH="`pwd`"
-echo $ROOT_PATH
+
+export NETWORK=reser
+if [ "${CI}" = "true" ]; then
+    export NETWORK=host
+fi
 
 docker run --rm  \
-   --network host \
+   --network ${NETWORK} \
    -v ${ROOT_PATH}/src/main/resources/db/migrations:/flyway/sql \
    -v ${ROOT_PATH}/src/main/resources/conf:/flyway/conf \
    boxfuse/flyway:5.1.4-alpine info migrate
