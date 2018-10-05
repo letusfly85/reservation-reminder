04e3c987f1560e09ed474e0a9d3cd7236c9f3bd6
letusfly85 <letusfly95@gmail.com>
2018-10-05 18:32:27 +0900

    fix: show info after migration

--- scripts/db-migrate.sh
+++ scripts/db-migrate.sh
@@ -11,4 +11,4 @@ docker run --rm  \
    --network ${NETWORK} \
    -v ${ROOT_PATH}/src/main/resources/db/migrations:/flyway/sql \
    -v ${ROOT_PATH}/src/main/resources/conf:/flyway/conf \
-   boxfuse/flyway:5.1.4-alpine info migrate
+   boxfuse/flyway:5.1.4-alpine info migrate info
