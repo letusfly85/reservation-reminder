29fc678fff30e3f97426129e01fab453575477d1
letusfly85 <letusfly95@gmail.com>
2018-10-05 11:42:48 +0900

    feat: use network host

--- scripts/db-migrate.sh
+++ scripts/db-migrate.sh
@@ -4,7 +4,7 @@ export ROOT_PATH="`pwd`"
 echo $ROOT_PATH
 
 docker run --rm  \
-   --net==host \
+   --network host \
    -v ${ROOT_PATH}/src/main/resources/db/migrations:/flyway/sql \
    -v ${ROOT_PATH}/src/main/resources/conf:/flyway/conf \
    boxfuse/flyway:5.1.4-alpine info migrate
