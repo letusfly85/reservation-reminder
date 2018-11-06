eaff7804b15b60a641d2bd7343b5464081ab6c57
letusfly85 <letusfly95@gmail.com>
2018-11-06 20:04:14 +0900

    feat: add job transaction query by statusName

--- src/main/scala/io/wonder/soft/reser/domain/repository/JobTransactionRepository.scala
+++ src/main/scala/io/wonder/soft/reser/domain/repository/JobTransactionRepository.scala
@@ -1,5 +1,11 @@
 package io.wonder.soft.reser.domain.repository
 
+import io.wonder.soft.reser.domain.entity.JobTransactionEntity
+
+import scala.concurrent.Future
+
 trait JobTransactionRepository {
+
+  def findByStatus(statusName: String): Future[List[JobTransactionEntity]]
 
 }
--- src/main/scala/io/wonder/soft/reser/domain/repository/impl/JobTransactionRepositoryImpl.scala
+++ src/main/scala/io/wonder/soft/reser/domain/repository/impl/JobTransactionRepositoryImpl.scala
@@ -1,7 +1,30 @@
 package io.wonder.soft.reser.domain.repository.impl
 
+import cats.data._
+import cats.implicits._
+
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
+import io.getquill._
+
+import scala.util.{Failure, Success, Try}
+
+import io.wonder.soft.reser.infra.DBConfig
+
+import io.wonder.soft.reser.domain.entity.JobTransactionEntity
 import io.wonder.soft.reser.domain.repository.JobTransactionRepository
 
-class JobTransactionRepositoryImpl extends JobTransactionRepository {
+class JobTransactionRepositoryImpl extends DBConfig with JobTransactionRepository {
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  val transactions = quote {
+    querySchema[JobTransactionEntity]("job_transactions", _.jobStatus -> "job_status")
+  }
+
+  def findByStatus(statusName: String): Future[List[JobTransactionEntity]] = {
+    this.run(quote {
+      transactions.filter(u => u.jobStatus == lift(statusName))
+    })
+  }
 
 }
