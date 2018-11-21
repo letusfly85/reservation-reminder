1149dac3cd79f5352de657c314671827ad48bb44
letusfly85 <letusfly95@gmail.com>
2018-11-22 08:27:34 +0900

    feat: add Docker service class

---
+++ src/main/scala/io/wonder/soft/reser/infra/DockerService.scala
@@ -0,0 +1,23 @@
+package io.wonder.soft.reser.infra
+
+import com.spotify.docker.client.DefaultDockerClient
+import java.net.URI
+
+/**
+  *
+  *
+  * refs: https://github.com/spotify/docker-client/blob/master/docs/user_manual.md#creating-a-docker-client
+  */
+class DockerService {
+
+  def generateClient(host: String, port: Int) = {
+    val docker = DefaultDockerClient
+      .builder
+      .uri(URI.create(s"http://${host}:${port}"))
+      // .dockerCertificates(new DockerCertificates(Paths.get("/Users/rohan/.docker/boot2docker-vm/")))
+      .build
+
+    docker
+  }
+
+}
