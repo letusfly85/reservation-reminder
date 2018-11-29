c8314974e9686e7aef4fc2f192c5cf1656e75a40
letusfly85 <letusfly95@gmail.com>
2018-11-29 22:47:17 +0900

    feat: add description

--- src/main/scala/io/wonder/soft/reser/infra/DockerService.scala
+++ src/main/scala/io/wonder/soft/reser/infra/DockerService.scala
@@ -8,13 +8,29 @@ import akka.actor.ActorRef
 /**
   *
   *
+  * ```/etc/sysconfig/docker
+  * # The max number of open files for the daemon itself, and all
+  * # running containers.  The default value of 1048576 mirrors the value
+  * # used by the systemd service unit.
+  * DAEMON_MAXFILES=1048576
+  *
+  * # Additional startup options for the Docker daemon, for example:
+  * # OPTIONS="--ip-forward=true --iptables=true"
+  * # By default we limit the number of open files per container
+  * OPTIONS="--default-ulimit nofile=1024:4096 -H tcp://0.0.0.0:2376 -H unix:///var/run/docker.sock"
+  *
+  * # How many seconds the sysvinit script waits for the pidfile to appear
+  * # when starting the daemon.
+  * DAEMON_PIDFILE_TIMEOUT=10
+  * ```
+  *
   * refs: https://github.com/spotify/docker-client/blob/master/docs/user_manual.md#creating-a-docker-client
   */
 class DockerService {
 
   private var client: DefaultDockerClient = _
 
-  def generateClient(host: String = "0.0.0.0", port: Int = 2376) = {
+  def generateClient(host: String = "0.0.0.0", port: Int = 2375) = {
     val docker = DefaultDockerClient
       .builder
       .uri(URI.create(s"http://${host}:${port}"))
