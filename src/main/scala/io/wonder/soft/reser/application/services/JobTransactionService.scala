2e424c34b655bfaebb5ef6a4429bb56023f4f017
letusfly85 <letusfly95@gmail.com>
2018-12-07 09:32:46 +0900

    feat: add run actors

--- src/main/scala/io/wonder/soft/reser/application/services/JobTransactionService.scala
+++ src/main/scala/io/wonder/soft/reser/application/services/JobTransactionService.scala
@@ -3,12 +3,20 @@ package io.wonder.soft.reser.application.services
 import io.wonder.soft.reser.domain.entity.JobTransactionEntity
 import io.wonder.soft.reser.domain.repository.JobTransactionRepository
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
-class JobTransactionService(jobTransactionRepository: JobTransactionRepository) {
+class JobTransactionService(jobTransactionRepository: JobTransactionRepository)(implicit executionContext: ExecutionContext) {
 
   def searchByStatus(statusName: String): Future[List[JobTransactionEntity]] = jobTransactionRepository.findByStatus(statusName)
 
   def create(entity: JobTransactionEntity) = jobTransactionRepository.create(entity)
+
+  def runScheduleActors = {
+    val transactions = this.searchByStatus("PENDING")
+    transactions.map(ts => ts.foreach { transaction =>
+      //TODO run actor
+      transaction
+    })
+  }
 
 }
