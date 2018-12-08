7de5f65b300bb0e0fd56d2b83493a93b7da908bc
letusfly85 <letusfly95@gmail.com>
2018-12-08 17:47:23 +0900

    feat: :sparkles: add runPendingJobs method

--- src/main/scala/io/wonder/soft/reser/application/services/JobTransactionService.scala
+++ src/main/scala/io/wonder/soft/reser/application/services/JobTransactionService.scala
@@ -1,22 +1,46 @@
 package io.wonder.soft.reser.application.services
 
+import java.util.Date
+
+import akka.actor.{ActorSystem, Props}
+import akka.stream.{ActorMaterializer, Materializer}
 import io.wonder.soft.reser.domain.entity.JobTransactionEntity
-import io.wonder.soft.reser.domain.repository.JobTransactionRepository
+import io.wonder.soft.reser.domain.job.{SimpleJobExecutor, SimpleJobGenerator}
+import io.wonder.soft.reser.domain.repository.{JobTransactionRepository, ReserveRepository}
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{Await, ExecutionContext, ExecutionContextExecutor, Future}
+import scala.concurrent.duration._
 
-class JobTransactionService(jobTransactionRepository: JobTransactionRepository)(implicit executionContext: ExecutionContext) {
+class JobTransactionService
+ (jobTransactionRepository: JobTransactionRepository,
+  reserveRepository: ReserveRepository)
+ (implicit executionContext: ExecutionContext) {
+
+  implicit val system: ActorSystem = ActorSystem("job-transaction")
+  implicit val executor: ExecutionContextExecutor = system.dispatcher
+  implicit val materializer: Materializer = ActorMaterializer()
 
   def searchByStatus(statusName: String): Future[List[JobTransactionEntity]] = jobTransactionRepository.findByStatus(statusName)
 
   def create(entity: JobTransactionEntity) = jobTransactionRepository.create(entity)
 
-  def runScheduleActors = {
-    val transactions = this.searchByStatus("PENDING")
-    transactions.map(ts => ts.foreach { transaction =>
-      //TODO run actor
-      transaction
-    })
+  def runPendingJobs = {
+    val futureTransactions = this.searchByStatus("PENDING")
+
+    val transactions = Await.result(futureTransactions, 5.seconds)
+    transactions.foreach {transaction =>
+      val reservedId = transaction.reserveId
+      reserveRepository.find(reservedId) match {
+        case Some(reserveEntity) =>
+          // val command = reserveEntity.command
+          val command = s"echo ${reserveEntity.description}"
+          val job = SimpleJobGenerator.generateJob(reserveEntity.name, "test", command)
+          val trigger = SimpleJobGenerator.generateTrigger(reserveEntity.name, "test", new Date(), 1, 0)
+
+          SimpleJobExecutor.startSchedule(job, trigger)
+      }
+
+    }
   }
 
 }
