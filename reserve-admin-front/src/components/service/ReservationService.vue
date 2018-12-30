f2499186f69e29bceb72e8aba6efab1f45453b1c
letusfly85 <letusfly95@gmail.com>
2018-12-30 11:16:48 +0900

    feat: :sparkles: add search reservations

--- reserve-admin-front/src/components/service/ReservationService.vue
+++ reserve-admin-front/src/components/service/ReservationService.vue
@@ -1,10 +1,19 @@
 <script>
 import ApiClient from './ApiClient'
 
+const pathPrefix = '/api/v1/reserves'
 export default {
   name: 'ReservationService',
+  findReservations: function (userId) {
+    let path = `${pathPrefix}?userId=${userId}`
+    ApiClient.search(path, (response) => {
+      console.log(response)
+    }, (error) => {
+      console.error(error)
+    })
+  },
   createReservation: function (userId, params) {
-    let path = `/api/v1/reserves?userId=${userId}`
+    let path = `${pathPrefix}?userId=${userId}`
     ApiClient.create(path, params, (response) => {
       console.log(response)
     }, (error) => {
